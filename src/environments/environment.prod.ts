fa4ca31b5ebb4f3ac39c79173a4d9418405e67ba
Joel Stevick <joelstevick@outlook.com>
2024-10-10 14:03:38 -0400

    fix url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
   keycloak: {
-    url: 'http://localhost:8080/auth',
+    url: 'http://localhost:8080',
     realm: 'my-realm',
     clientId: 'my-app',
     clientSecret: 'OdESmUxkGKaiSxeeP0TI9eH9HMK01Det',  // Add your client secret here
