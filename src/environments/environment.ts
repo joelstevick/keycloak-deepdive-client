2e8eb00bea97ba19098df0550722510206d6cd95
Joel Stevick <joelstevick@outlook.com>
2024-10-22 09:19:39 -0400

    client secret not used for public clients

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,6 @@ export const environment = {
     url: 'http://localhost:8080',
     realm: 'my-realm',
     clientId: 'my-app',
-    clientSecret: 'OdESmUxkGKaiSxeeP0TI9eH9HMK01Det',  // Add your client secret here
   },
   api_url: 'http://localhost:8200',  // Adjust this according to your API
 };
